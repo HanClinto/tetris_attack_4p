9b7d14fca7001b77d404a48a7ec4edcd429b3e8d
Clint Herron <hanclinto@gmail.com>
2026-08-04 21:57:45 -0400

    Persist virtual player state
    Add a traced 19-word scalar sidecar and route P3 processed input through the native P2 slots. Verify an injected P3 A press changes the virtual four-plane board while all P2 plane, scalar, and input state restores exactly.

--- romhack/experiments/context-dispatch-probe.asm
+++ romhack/experiments/context-dispatch-probe.asm
@@ -32,6 +32,12 @@ ContextDispatchPreHook:
     ldx #$0100
     ldy #$0800
     jsr SaveNativeSlotBlockMove
+    ldx #$0100
+    jsr SaveP2ScalarState
+    ldx #$0000
+    jsr SaveP2ScalarState
+    jsr LoadP3ScalarState
+    jsr LoadP3InputState
 
 .done:
     rep #$30
@@ -62,6 +68,9 @@ ContextDispatchPostHook:
     ldx #$0100
     ldy #$0800
     jsr SaveNativeSlotBlockMove
+    jsr SaveP3ScalarState
+    jsr RestoreP2ScalarState
+    jsr RestoreP2InputState
     ldx #$0000
     ldy #$0100
     jsr LoadNativeSlotBlockMove
@@ -139,5 +148,135 @@ LoadNativeSlotBlockMove:
     mvn $7E,$7F
     rts
 
-assert ContextDispatchPostComplete == $A08BB6
-assert pc() <= $A08E00
+org $A08D00
+SaveP2ScalarState:
+    lda.l $7E03F0
+    sta.l $7F0C00,x
+    lda.l $7E0402
+    sta.l $7F0C02,x
+    lda.l $7E0426
+    sta.l $7F0C04,x
+    lda.l $7E042A
+    sta.l $7F0C06,x
+    lda.l $7E042E
+    sta.l $7F0C08,x
+    lda.l $7E0442
+    sta.l $7F0C0A,x
+    lda.l $7E0446
+    sta.l $7F0C0C,x
+    lda.l $7E044A
+    sta.l $7F0C0E,x
+    lda.l $7E044E
+    sta.l $7F0C10,x
+    lda.l $7E0452
+    sta.l $7F0C12,x
+    lda.l $7E0456
+    sta.l $7F0C14,x
+    lda.l $7E045A
+    sta.l $7F0C16,x
+    lda.l $7E045E
+    sta.l $7F0C18,x
+    lda.l $7E046E
+    sta.l $7F0C1A,x
+    lda.l $7E0472
+    sta.l $7F0C1C,x
+    lda.l $7E04C0
+    sta.l $7F0C1E,x
+    lda.l $7E04EA
+    sta.l $7F0C20,x
+    lda.l $7E04F6
+    sta.l $7F0C22,x
+    lda.l $7E04FA
+    sta.l $7F0C24,x
+    rts
+
+LoadP3ScalarState:
+    ldx #$0000
+    bra LoadP2ScalarState
+
+RestoreP2ScalarState:
+    ldx #$0100
+LoadP2ScalarState:
+    lda.l $7F0C00,x
+    sta.l $7E03F0
+    lda.l $7F0C02,x
+    sta.l $7E0402
+    lda.l $7F0C04,x
+    sta.l $7E0426
+    lda.l $7F0C06,x
+    sta.l $7E042A
+    lda.l $7F0C08,x
+    sta.l $7E042E
+    lda.l $7F0C0A,x
+    sta.l $7E0442
+    lda.l $7F0C0C,x
+    sta.l $7E0446
+    lda.l $7F0C0E,x
+    sta.l $7E044A
+    lda.l $7F0C10,x
+    sta.l $7E044E
+    lda.l $7F0C12,x
+    sta.l $7E0452
+    lda.l $7F0C14,x
+    sta.l $7E0456
+    lda.l $7F0C16,x
+    sta.l $7E045A
+    lda.l $7F0C18,x
+    sta.l $7E045E
+    lda.l $7F0C1A,x
+    sta.l $7E046E
+    lda.l $7F0C1C,x
+    sta.l $7E0472
+    lda.l $7F0C1E,x
+    sta.l $7E04C0
+    lda.l $7F0C20,x
+    sta.l $7E04EA
+    lda.l $7F0C22,x
+    sta.l $7E04F6
+    lda.l $7F0C24,x
+    sta.l $7E04FA
+    rts
+
+SaveP3ScalarState:
+    ldx #$0000
+    jsr SaveP2ScalarState
+    rts
+
+LoadP3InputState:
+    lda.l $7E00B5
+    sta.l $7F0D40
+    lda.l $7E00B9
+    sta.l $7F0D42
+    lda.l $7E00BD
+    sta.l $7F0D44
+    lda.l $7E00C7
+    sta.l $7F0D46
+    lda.l $7E00CD
+    sta.l $7F0D48
+    lda.l $7FFE10
+    sta.l $7E00B5
+    lda.l $7FFE12
+    sta.l $7E00B9
+    lda.l $7FFE14
+    sta.l $7E00BD
+    lda.l $7FFE16
+    sta.l $7E00C7
+    lda.l $7FFE18
+    sta.l $7E00CD
+    rts
+
+RestoreP2InputState:
+    lda.l $7F0D40
+    sta.l $7E00B5
+    lda.l $7F0D42
+    sta.l $7E00B9
+    lda.l $7F0D44
+    sta.l $7E00BD
+    lda.l $7F0D46
+    sta.l $7E00C7
+    lda.l $7F0D48
+    sta.l $7E00CD
+    rts
+
+assert ContextDispatchPostComplete == $A08BBF
+assert pc() <= $A09000
--- romhack/tests/mesen/context_dispatch_probe.lua
+++ romhack/tests/mesen/context_dispatch_probe.lua
@@ -3,11 +3,22 @@ local syntheticInput1 = 0
 local syntheticInput2 = 0
 local nativeBefore = nil
 local virtualAfter = nil
+local scalarBefore = nil
+local virtualScalarAfter = nil
+local inputBefore = nil
 local dispatcherEntries = 0
 local preHookEntries = 0
 local postHookEntries = 0
 
 local planeBases = { 0x0D7C, 0x0F7C, 0x117C, 0x137C }
+local scalarAddresses = {
+    0x03F0, 0x0402, 0x0426, 0x042A, 0x042E,
+    0x0442, 0x0446, 0x044A, 0x044E, 0x0452,
+    0x0456, 0x045A, 0x045E, 0x046E, 0x0472,
+    0x04C0, 0x04EA, 0x04F6, 0x04FA,
+}
+local inputAddresses = { 0x00B5, 0x00B9, 0x00BD, 0x00C7, 0x00CD }
+local p3InputAddresses = { 0x1FE10, 0x1FE12, 0x1FE14, 0x1FE16, 0x1FE18 }
 
 emu.write(0x12000, 0, emu.memType.snesWorkRam)
 emu.write(0x12001, 0, emu.memType.snesWorkRam)
@@ -60,6 +71,31 @@ local function captureBacking(startAddress)
     return bytes
 end
 
+local function captureWords(addresses)
+    local bytes = {}
+    for _, address in ipairs(addresses) do
+        table.insert(bytes, emu.read(address, emu.memType.snesWorkRam))
+        table.insert(bytes, emu.read(address + 1, emu.memType.snesWorkRam))
+    end
+    return bytes
+end
+
+local function captureBackingBytes(startAddress, length)
+    local bytes = {}
+    for offset = 0, length - 1 do
+        bytes[offset + 1] = emu.read(
+            startAddress + offset,
+            emu.memType.snesWorkRam
+        )
+    end
+    return bytes
+end
+
+local function writeWord(address, value)
+    emu.write(address, value & 0xFF, emu.memType.snesWorkRam)
+    emu.write(address + 1, (value >> 8) & 0xFF, emu.memType.snesWorkRam)
+end
+
 local function setMenuInput()
     syntheticInput1 = 0
     syntheticInput2 = 0
@@ -100,11 +136,29 @@ emu.addMemoryCallback(function()
         captureBacking(0x10800)
     ) then
         emu.stop(2)
+    elseif not verifyBytes(
+        captureWords(scalarAddresses),
+        captureBackingBytes(0x10C00, #scalarAddresses * 2)
+    ) then
+        emu.stop(8)
+    elseif not verifyBytes(
+        captureWords(inputAddresses),
+        captureWords(p3InputAddresses)
+    ) then
+        emu.stop(9)
     end
 end, emu.callbackType.exec, 0x82A9C8)
 
 emu.addMemoryCallback(function()
     preHookEntries = preHookEntries + 1
+    if nativeBefore ~= nil and
+        emu.read(0x12000, emu.memType.snesWorkRam) == 0xA5 then
+        writeWord(0x1FE10, 0x0080)
+        writeWord(0x1FE12, 0x0080)
+        writeWord(0x1FE14, 0x0080)
+        writeWord(0x1FE16, 0x0000)
+        writeWord(0x1FE18, 0x0010)
+    end
 end, emu.callbackType.exec, 0xA08B00)
 
 emu.addMemoryCallback(function()
@@ -112,6 +166,7 @@ emu.addMemoryCallback(function()
     if nativeBefore ~= nil and
         emu.read(0x12001, emu.memType.snesWorkRam) == 0x5A then
         virtualAfter = captureNativeP2()
+        virtualScalarAfter = captureWords(scalarAddresses)
     end
 end, emu.callbackType.exec, 0xA08B80)
 
@@ -135,10 +190,26 @@ emu.addMemoryCallback(function()
         emu.stop(5)
     elseif not verifyBytes(virtualAfter, captureBacking(0x10800)) then
         emu.stop(6)
+    elseif not verifyBytes(scalarBefore, captureWords(scalarAddresses)) then
+        emu.stop(10)
+    elseif not verifyBytes(
+        scalarBefore,
+        captureBackingBytes(0x10D00, #scalarAddresses * 2)
+    ) then
+        emu.stop(11)
+    elseif not verifyBytes(
+        virtualScalarAfter,
+        captureBackingBytes(0x10C00, #scalarAddresses * 2)
+    ) then
+        emu.stop(12)
+    elseif not verifyBytes(inputBefore, captureWords(inputAddresses)) then
+        emu.stop(13)
+    elseif verifyBytes(nativeBefore, virtualAfter) then
+        emu.stop(14)
     else
         emu.stop(0)
     end
-end, emu.callbackType.exec, 0xA08BB6)
+end, emu.callbackType.exec, 0xA08BBF)
 
 emu.addEventCallback(function()
     frame = frame + 1
@@ -146,6 +217,8 @@ emu.addEventCallback(function()
 
     if frame == 4700 then
         nativeBefore = captureNativeP2()
+        scalarBefore = captureWords(scalarAddresses)
+        inputBefore = captureWords(inputAddresses)
         emu.write(0x12000, 0xA5, emu.memType.snesWorkRam)
     elseif frame >= 4780 then
         print(string.format(
